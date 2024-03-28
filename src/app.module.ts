5be16d31820415c98c0e03be3395370d781bc9da
EfimPolitis <efempolites@gmail.com>
2024-03-28 23:05:35 +0300

    add services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { EventModule } from './event/event.module';
+import { RequestModule } from './request/request.module';
+import { BookingModule } from './booking/booking.module';
+import { CommentModule } from './comment/comment.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, EventModule, RequestModule, BookingModule, CommentModule, ReportModule],
 })
 export class AppModule {}
